014802d876a4d919770dbfcba36edd6a6f9a4aa9
Hyago <hyagoassis@ice.ufjf.br>
2021-06-06 16:41:45 -0300

    Alterando repositorio

---
+++ Mr-Assis/core/database/QueryBuilder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Core\Database;
+
+use PDO;
+
+class QueryBuilder
+{
+
+    protected $pdo;
+
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+}
